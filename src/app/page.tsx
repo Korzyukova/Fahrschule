6294268cc3daae9602120ad43e732a35dd56d234
Tatiana Korzyukova <korzioukova@gmail.com>
2024-02-09 10:07:22 -0500

    header1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1 className="font">Home</h1>
       <Image src="/santa.webp" alt="Santa" width="500" height="500" />
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,30 @@
 import Image from 'next/image';
 
 export default function Header() {
+    const links = {
+        link1: '/',
+        link2: '/about-us',
+    
+        link3: '/contact-us',
+      }
     return (
-        <div>
-            <h1>Header</h1>
-            <Image src="/santa.webp" alt="Santa" width="500" height="500" />
+        <>
+        <header className='header'>
+            <Image className='logo' src="/logo.png" alt="logo" width="280" height="85" />
+            <div className="links">
+          <a className="link" href={links.link1}>
+            Home
+          </a>
+          <a className="link" href={links.link2}>
+            About Us
+          </a>
+          <a className="link" href={links.link3}>
+            Contact Us
+          </a>
         </div>
+        
+        </header>
+         <Image className="header-picture" alt="city" src="/Munich.png" width="280" height="85"/> 
+        </>
     )
 }
