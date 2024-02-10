a99513dcfb1838955d1210f3086a419c8d4ca053
Tatiana Korzyukova <korzioukova@gmail.com>
2024-02-09 20:22:01 -0500

    Home containers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,19 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className="homepage">
       <Header />
-      <h1 className="font">Home</h1>
-      <Image src="/santa.webp" alt="Santa" width="500" height="500" />
+      <div className="container-one">
+      <div className="red-square"></div>
+      {/* <h1 className="font">Home</h1>
+      <Image src="/santa.webp" alt="Santa" width="500" height="500" /> */}
+      </div>
+      <div className="container-two">
+        
+      {/* <h1 className="font">Home</h1>
+      <Image src="/santa.webp" alt="Santa" width="500" height="500" /> */}
+      </div>
+      
       <Footer />
     </div>
   );
