375e518c6836d0321bfdba0f43e654644f195b58
Tatiana Korzyukova <korzioukova@gmail.com>
2024-02-10 10:46:26 -0500

    home almost finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,12 +20,27 @@ A, A1, A2, B, B78, B197, B196
       <p className="square-text-four">Nachlassauf
  die Anmeldegebühr</p>
       </ul>
-
       </div>
-    
       </div>
       <div className="container-two">
-        
+        <div className="container-two-paragraphs">
+          <p>Wir bieten dir eine moderne und professionelle Ausbildung in Theorie und Praxis</p>
+          <ul>
+            <li>Lern-APP</li>
+            <li>Transparente Ausbildung anhand der Ausbildungsdiagrammkarte</li>
+            <li>Gruppenarbeit</li>
+            <li>Moderne Fahrzeuge</li>
+            <li>Prüfungssimulationen</li>
+            <li>Modernen Lernmethoden</li>
+          </ul>
+          <p>Du hast schon einen Führerschein, aber fühlst dich nicht sicher im Straßenverkehr?</p>
+          <p>Kein Problem! Wir begleiten dich zu mehr Kompetenz im Straßenverkehr egal ob mit dem PKW oder dem Motorrad,
+             damit du wieder sicher unterwegs bist</p>
+        </div>
+        <div className="container-two-years">
+          <p className="years">30</p>
+          <p className="experience">Jahre Erfahrung damit, Menschen dabei zu unterstützen, das Fahren zu erlernen.</p>
+        </div>
       </div>
       
       <Footer />
