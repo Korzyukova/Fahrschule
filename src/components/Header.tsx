a208d6fc8bb2696191645a0451a9e85b077cb1f9
Tatiana Korzyukova <korzioukova@gmail.com>
2024-02-11 18:01:20 -0500

    burger menu links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
     position: 'fixed' as 'fixed',
     left: isOpen ? '0' : '-100%', // Slide in from left
     top: '0',
-    width: '33%', // 33% of the viewport width
+    width: '50%', // 33% of the viewport width
     height: '100%', // 100% of the viewport height
     backgroundColor: '#98BEC4', // Menu background color
     transition: 'left 0.3s ease', // Smooth transition for sliding effect
