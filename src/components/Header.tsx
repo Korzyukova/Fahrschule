c723690ce3c0ff145af7b9809043028c7bc5fb31
Tatiana Korzyukova <korzioukova@gmail.com>
2024-02-11 21:04:54 -0500

    smooth transition

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,19 +11,6 @@ export default function Header() {
     setIsOpen(!isOpen);
   };
 
-  // Inline styles for the burger menu
-  const menuStyles = {
-    position: 'fixed' as 'fixed',
-    left: isOpen ? '0' : '-100%', // Slide in from left
-    top: '0',
-    width: '50%', // 33% of the viewport width
-    height: '100%', // 100% of the viewport height
-    backgroundColor: '#98BEC4', // Menu background color
-    transition: 'left 0.3s ease', // Smooth transition for sliding effect
-    padding: '20px', // Inner spacing
-    zIndex: 10,
-  };
-
   return (
     <>
       <header className='header'>
@@ -36,13 +23,11 @@ export default function Header() {
         <button onClick={toggleMenu} className="burger-button">☰</button>
       </header>
       <Image className="header-picture" alt="city" src={Munich} width="280" height="85" />
-      {isOpen && (
-        <div id="burger-menu" style={menuStyles}>
-          <a href="/Fahrschule">HOME</a>
-          <a href="/Fahrschule/about">ÜBER UNS</a>
-          <a href="/Fahrschule/contact">KONTAKT</a>
-        </div>
-      )}
+      <div id="burger-menu" className={`${isOpen ? 'show' : 'hide'}`}>
+        <a href="/Fahrschule">HOME</a>
+        <a href="/Fahrschule/about">ÜBER UNS</a>
+        <a href="/Fahrschule/contact">KONTAKT</a>
+      </div>
     </>
   );
 }
